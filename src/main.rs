62743a48d5b3fa161fc753481fb27d0d78b14aaf
mexus <gilaldpellaeon@gmail.com>
2018-03-04 12:32:36 +0100

    Got rid of `Box`es

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,19 @@ fn main() {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
-    let reading = Box::new(
-        io::lines(BufReader::new(tokio_stdin_stdout::stdin(0)))
-            .for_each(|s| {
-                println!("Received: {}", s);
-                Ok(())
-            })
-            .into_future()
-            .map_err(|e| println!("Error while reading: {}", e)),
-    );
-    let ctrl_c = Box::new(
-        tokio_signal::ctrl_c(&handle)
-            .flatten_stream()
-            .map_err(|e| println!("Error while reading: {}", e))
-            .take(1)
-            .into_future()
-            .map(|_| println!("Received ctrl_c")),
-    );
+    let reading = io::lines(BufReader::new(tokio_stdin_stdout::stdin(0)))
+        .for_each(|s| {
+            println!("Received: {}", s);
+            Ok(())
+        })
+        .into_future()
+        .map_err(|e| println!("Error while reading: {}", e));
+    let ctrl_c = tokio_signal::ctrl_c(&handle)
+        .flatten_stream()
+        .map_err(|e| println!("Error while reading: {}", e))
+        .take(1)
+        .into_future()
+        .map(|_| println!("Received ctrl_c"));
 
     match core.run(ctrl_c.select2(reading)) {
         _ => {}
